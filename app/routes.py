5b25a04abcc723d3a07ecf8668075bb2e82af640
Jean Mario <jeanmario98763@gmail.com>
2023-12-20 19:17:16 -0600

    Chapter 2 Templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    usuario = {'nombre' : 'Jean'}
+    Posts = [
+        {
+            'autor': {'usuarioNombre': 'Hugo'},
+            'contenido': 'Beautiful day in portland!'
+        },
+        {
+            'autor': {'usuarioNombre': 'Pedro'},
+            'contenido': 'the avengers movie was so cool'
+        }
+    ]
+    return render_template('index.html', title='home', usuario=usuario, Posts=Posts)
