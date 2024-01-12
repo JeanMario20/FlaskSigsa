cb8119a2d5504944663340774dd8fba70a911f53
Jean Mario <jeanmario98763@gmail.com>
2024-01-11 23:02:39 -0600

    Chapter 13 I18n and L10n

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_babel import Babel
 from flask_login import LoginManager
 from flask_mail import Mail, Message
 from flask_moment import Moment
+from flask_babel import lazy_gettext as _l
 import logging
 from logging.handlers import RotatingFileHandler, SMTPHandler
 import os
+#flask_env
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,10 +22,12 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 login = LoginManager(app)
+login.login_message = _l('porfavor inicia sesion para acceder a esta pagina')
 mail = Mail()
 login.login_view = 'login'
 mail.init_app(app)
 moment = Moment(app)
+babel = Babel(app, locale_selector = get_locale)
 
 
 if not app.debug:
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, g
+from flask_babel import get_locale
 from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
 from app import app, db
@@ -7,6 +8,13 @@ from app.forms import LoginForm, RegistrarForm, EditarPerfilForm, EmptyForm, Pos
 from app.email import send_password_reset_email
 from urllib.parse import urlsplit
 from datetime import datetime, timezone
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.ultima_conexion = datetime.now(timezone.utc)
+        db.session.commit()
+    g.locale = str(get_locale())
 
 
 @app.route('/', methods = ['GET', 'POST'])
