3edbbf0da6de6bd97575efb11b0c5cbd954cc28c
Jean Mario <jeanmario98763@gmail.com>
2024-02-02 19:32:41 -0600

    Chapter 21 User Notifications

--- app/main/routes.py
+++ app/main/routes.py
@@ -10,6 +10,8 @@ from app.main.forms import EditarPerfilForm, EmptyForm, PostForm, SearchForm
 from app.models import Usuario, Post
 from app.translate import translate
 from app.main import bp
+from app.main.forms import MessageForm
+from app.models import Message, Notification
 
 @bp.before_request
 def before_request():
@@ -61,7 +63,6 @@ def explore():
 @bp.route('/usuario/<nombreUsuario>')
 @login_required
 def usuario(nombreUsuario):
-    
     usuario = db.first_or_404(sa.select(Usuario).where(Usuario.nombreUsuario == nombreUsuario))
     page = request.args.get('page', 1, type = int)
     query = usuario.posts.select().order_by(Post.tiempo_publicado.desc())
@@ -158,3 +159,46 @@ def search():
         if page > 1 else None
     return render_template('buscador.html', title=_('Search'), posts=posts,
                            next_url=next_url, prev_url=prev_url)
+
+@bp.route('/send_message/<recipient>', methods=['GET', 'POST'])
+@login_required
+def send_message(recipient):
+    usuario = db.first_or_404(sa.select(Usuario).where(Usuario.nombreUsuario == recipient))
+    form = MessageForm()
+    if form.validate_on_submit():
+        msg = Message(autor=current_user, recipient=usuario,body=form.message.data)
+        db.session.add(msg)
+        usuario.add_notification('unread_message_count', usuario.unread_message_count())
+        db.session.commit()
+        flash(_('Tu mensaje a sido enviado.'))
+        return redirect(url_for('main.usuario', nombreUsuario=recipient))
+    return render_template('send_message.html', title=_('Enviar mensaje'),form=form, recipient=recipient)
+
+@bp.route('/messages')
+@login_required
+def messages():
+    current_user.last_message_read_time = datetime.now(timezone.utc)
+    current_user.add_notification('unread_message_count', 0)
+    db.session.commit()
+    page = request.args.get('page', 1, type=int)
+    query = current_user.messages_received.select().order_by(
+        Message.timestamp.desc())
+    messages = db.paginate(query, page=page,per_page=current_app.config['POSTS_PER_PAGE'],error_out=False)
+    next_url = url_for('main.messages', page=messages.next_num) \
+        if messages.has_next else None
+    prev_url = url_for('main.messages', page=messages.prev_num) \
+        if messages.has_prev else None
+    return render_template('messages.html', messages=messages.items,next_url=next_url, prev_url=prev_url)
+
+@bp.route('/notifications')
+@login_required
+def notifications():
+    since = request.args.get('since', 0.0, type=float)
+    query = current_user.notifications.select().where(
+        Notification.timestamp > since).order_by(Notification.timestamp.asc())
+    notifications = db.session.scalars(query)
+    return [{
+        'name': n.name,
+        'data': n.get_data(),
+        'timestamp': n.timestamp
+    } for n in notifications]
--- sigsaBlog.py
+++ sigsaBlog.py
@@ -1,11 +1,11 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import create_app, db
-from app.models import Usuario, Post
+from app.models import Usuario, Post, Message, Notification
 from app import cli
 
 app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'sa': sa, 'so':so, 'db': db, 'Usuario': Usuario, 'Post':Post}
+    return {'sa': sa, 'so':so, 'db': db, 'Usuario': Usuario, 'Post':Post,'Message': Message, 'Notification': Notification}
